7f8cf1b07188a025631f0da5e91a35a3d7045d3e
Atle Prange <atle.prange@kantega.no>
2015-03-26 13:19:04 +0100

    Added exception handling

--- src/main/java/no/kantega/concurrent/Async.java
+++ src/main/java/no/kantega/concurrent/Async.java
@@ -1,19 +1,17 @@
 package no.kantega.concurrent;
 
 import fj.F;
-import fj.P;
+import fj.Function;
 import fj.P2;
 import fj.Unit;
 import fj.control.parallel.Actor;
 import fj.control.parallel.Strategy;
 import fj.data.Either;
 import fj.data.Option;
-import fj.data.Stream;
-import fj.data.Validation;
 import fj.function.Effect1;
+import no.kantega.effect.Tried;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -38,48 +36,41 @@ public abstract class Async<A> {
      * Creates an Async that is resolved by a callback.
      *
      * @param handler The handler that must execute the task, and eventually call the resolver to resolve
-     * the Async task.
-     * @param <A> The type of the value the task creates asyncronically.
+     *                the Async task.
+     * @param <A>     The type of the value the task creates asyncronically.
      * @return An async that eventually will produce result.
      */
     public static <A> Async<A> async(Task<A> handler) {
         return new Async<A>() {
             @Override
-            public void execute(final Effect1<A> completeHandler) {
-                out.println("Running execute");
-                handler.run(completeHandler::f);
+            public void execute(final Effect1<Tried<A>> completeHandler) {
+                out.println( "Running execute" );
+                try {
+                    handler.run( completeHandler::f );
+                } catch (Throwable t) {
+                    completeHandler.f( Tried.fail( t ) );
+                }
             }
         };
     }
 
 
+    public static <A> Async<A> fail(Throwable t){
+        return async( aresolver -> aresolver.resolve( Tried.fail( t ) ) );
+    }
+
+    public static <A> Async<A> call(final Supplier<A> task) {
+        return async( validationResolver -> validationResolver.resolve( Tried.value( task.get() ) ) );
+    }
+
     /**
-     * Creates and Async task that calls the Callable task some time in the future, in a thread specified by the strategy.
-     *
-     * @param task The Callable task to call
-     * @param <A> Type type of the value the callable yields
+     * Puts the argument into a Async.
      */
-    public static <A> Async<Validation<Exception, A>> callV(final Supplier<A> task) {
-        return async(validationResolver -> {
-            try {
-                A a = task.get();
-                validationResolver.resolve(Validation.<Exception, A>success(a));
-            } catch (Exception e) {
-                validationResolver.resolve(Validation.<Exception, A>fail(e));
-            }
-        });
-    }
-
-    public static <A> Async<A> call(final Supplier<A> task){
-        return async(validationResolver -> validationResolver.resolve(task.get()));
-    }
-
-    /** Puts the argument into a Async. */
     public static <A> Async<A> now(final A a) {
-        return async(aResolver -> {
-            out.println("Running immediately");
-            aResolver.resolve(a);
-        });
+        return async( aResolver -> {
+            out.println( "Running immediately" );
+            aResolver.resolve( Tried.value( a ) );
+        } );
     }
 
     /**
@@ -87,12 +78,12 @@ public abstract class Async<A> {
      * Uses the first Asyncs startegy to call the continuation. The tasks are run in parallell if permitted by the executor.
      */
     public static <A, B> Async<P2<A, B>> and(final Async<A> one, final Async<B> other) {
-        return async(p2Resolver -> {
+        return async( p2Resolver -> {
             EffectSynchronizer<A, B> effectSynchronizer =
-                    new EffectSynchronizer<>(p2Resolver);
-            one.execute(effectSynchronizer.leftE());
-            other.execute(effectSynchronizer.rightE());
-        });
+                    new EffectSynchronizer<>( p2Resolver );
+            one.execute( effectSynchronizer.leftE() );
+            other.execute( effectSynchronizer.rightE() );
+        } );
     }
 
 
@@ -102,122 +93,97 @@ public abstract class Async<A> {
     public Async<A> delay(Duration duration, final ScheduledExecutorService executorService) {
         return new Async<A>() {
             @Override
-            public void execute(final Effect1<A> completeHandler) {
-                out.println("Scheduling task");
-                executorService.schedule(() -> Async.this.execute(completeHandler), duration.toMillis(), TimeUnit.MILLISECONDS);
+            public void execute(final Effect1<Tried<A>> completeHandler) {
+                out.println( "Scheduling task" );
+                executorService.schedule( () -> Async.this.execute( completeHandler ), duration.toMillis(), TimeUnit.MILLISECONDS );
             }
         };
     }
 
-    /**
-     * Runs immediately, and the retries with the given delays until the predicate yields true or the stream of delays ends.
-     */
-    public static <A> Async<Option<A>> retryUntil(Stream<Duration> retries, F<RetryContext<A>, Boolean> predicate, final ScheduledExecutorService executorService, Task<A> handler) {
-        return retryUntilRec(() -> retries, predicate, Instant.now(), 1, executorService, handler);
-    }
-
-    private static <A> Async<Option<A>> retryUntilRec(Supplier<Stream<Duration>> lazyRetries, F<RetryContext<A>, Boolean> predicate, Instant start, int tryNumber, final ScheduledExecutorService executorService,final Task<A> handler) {
-
-        return new Async<Option<A>>() {
-            @Override
-            public void execute(final Effect1<Option<A>> completeHandler) {
-
-                Resolver<A> resolver = (A a) -> {
-                    if (predicate.f(new RetryContext<>(a, tryNumber, Duration.between(start, Instant.now())))) {
-                        completeHandler.f(Option.fromNull(a));
-                    } else {
-                        Stream<Duration> evaluatedRetries = lazyRetries.get();
-                        Option<Duration> maybeDelay = evaluatedRetries.toOption();
-
-                        for (Duration delay : maybeDelay) {
-                            out.println("Scheduling task in retry");
-                            executorService.schedule(() -> retryUntilRec(evaluatedRetries.tail()::_1, predicate, start, tryNumber + 1,executorService,handler).execute( completeHandler ), delay.toMillis(), TimeUnit.MILLISECONDS);
-                        }
-                        if (maybeDelay.isNone()) {
-                            out.println("No result");
-                            completeHandler.f(Option.none());
-                        }
-                    }
-                };
-
-              handler.run(resolver);
-            }
-        };
-    }
 
     /**
      * Applies f to the result of this Async
      *
-     * @param f the function to apply
+     * @param f   the function to apply
      * @param <B> the type the function f produces
      * @return An Async with the result transformed.
      */
     public <B> Async<B> map(final F<A, B> f) {
-        return async(resolver -> Async.this.execute(a -> resolver.resolve(f.f(a))));
+        return async( resolver -> Async.this.execute( a -> resolver.resolve( a.map(f) ) ) );
     }
 
     /**
-     * Bind the next Aync to this async.
+     * Bind the next Aync to this async. If the first async fails the second is not run. If the second fails the result is a fail.
      *
-     * @param f a function that takes the result of this async, and provides the next async.
+     * @param f   a function that takes the result of this async, and provides the next async.
      * @param <B> the type the next async produces.
      * @return An Async that first executes this task, and then the next task when this task is finished.
      */
     public <B> Async<B> flatMap(final F<A, Async<B>> f) {
-        return async(resolver -> Async.this.execute(a -> f.f(a).execute(resolver::resolve)));
+        return async( resolver -> Async.this.execute( a -> a.map(f).fold( Async::fail, Function.identity() ).execute( resolver::resolve ) ) );
     }
 
 
-    /** Run the other Async task after this task completes, disregarding the outcome of the first Async. */
+    /**
+     * Run the other Async task after this task completes, disregarding the outcome of the first Async.
+     */
     public <B> Async<B> andThen(final Async<B> other) {
-        return flatMap(a -> other);
+        return flatMap( a -> other );
     }
 
 
+    public Option<Tried<A>> await(Duration timeout) {
+        CountDownLatch latch = new CountDownLatch( 1 );
 
-    public Option<A> await(Duration timeout){
-        CountDownLatch latch = new CountDownLatch(1);
-
-        AtomicReference<A> ref = new AtomicReference<>();
+        AtomicReference<Tried<A>> ref = new AtomicReference<>();
 
         execute( a -> {
-            ref.set(a);
+            ref.set( a );
             latch.countDown();
         } );
         try {
-            latch.await(timeout.toMillis(),TimeUnit.MILLISECONDS);
+            latch.await( timeout.toMillis(), TimeUnit.MILLISECONDS );
         } catch (InterruptedException e) {
-            return Option.fromNull(ref.get());
+            return Option.fromNull( ref.get() );
         }
-        return Option.fromNull(ref.get());
+        return Option.fromNull( ref.get() );
     }
 
 
-    /** Runs the task*/
-    public abstract void execute(Effect1<A> completeHandler);
+    /**
+     * Runs the task
+     */
+    public abstract void execute(Effect1<Tried<A>> completeHandler);
 
 
-    /** Interface for tasks that are to be run asyncronusly with a callback to resolve the Async. */
+    /**
+     * Interface for tasks that are to be run asyncronusly with a callback to resolve the Async.
+     */
     public static interface Task<A> {
         public void run(Resolver<A> resolver);
     }
 
-    /** Interface for the callback. Resolves the async
-     *  The resolver passes the result object to the client.
+    /**
+     * Interface for the callback. Resolves the async
+     * The resolver passes the result object to the client.
      */
     public static interface Resolver<A> {
 
         /**
          * Passes the result over to the client. The client is run in the same thread as the caller of resolve
+         *
          * @param result
          */
-        public void resolve(A result);
+        public void resolve(Tried<A> result);
+
     }
 
     public static class RetryContext<A> {
 
         public final A value;
+
         public final int retries;
+
         public final Duration elapsed;
 
         public RetryContext(final A value, final int retries, final Duration elapsed) {
@@ -233,48 +199,49 @@ public abstract class Async<A> {
     private static class EffectSynchronizer<A, B> {
 
 
-        final Actor<Either<A, B>> actor;
+        final Actor<Either<Tried<A>, Tried<B>>> actor;
 
-        volatile Option<A> aValue =
+        volatile Option<Tried<A>> aValue =
                 Option.none();
 
-        volatile Option<B> bValue =
+        volatile Option<Tried<B>> bValue =
                 Option.none();
 
         EffectSynchronizer(final Resolver<P2<A, B>> targetEffect) {
             //Actor that ensures no sharing of state bewteen threads
             actor =
-                    Actor.actor(Strategy.<Unit>seqStrategy(), new Effect1<Either<A, B>>() {
+                    Actor.actor( Strategy.<Unit>seqStrategy(), new Effect1<Either<Tried<A>, Tried<B>>>() {
                         @Override
-                        public void f(final Either<A, B> value) {
+                        public void f(final Either<Tried<A>, Tried<B>> value) {
                             if (value.isLeft()) {
-                                aValue = Option.some(value.left().value());
+                                aValue = Option.some( value.left().value() );
 
                                 if (bValue.isSome()) {
-                                    targetEffect.resolve(P.p(aValue.some(), bValue.some()));
+                                    targetEffect.resolve( aValue.some().and( bValue.some() ) );
                                 }
-                            } else {
-                                bValue = Option.some(value.right().value());
+                            }
+                            else {
+                                bValue = Option.some( value.right().value() );
 
                                 if (aValue.isSome()) {
-                                    targetEffect.resolve(P.p(aValue.some(), bValue.some()));
+                                    targetEffect.resolve( aValue.some().and( bValue.some() ) );
                                 }
                             }
                         }
-                    });
+                    } );
         }
 
 
-        private void handle(Either<A, B> value) {
-            actor.act(value);
+        private void handle(Either<Tried<A>, Tried<B>> value) {
+            actor.act( value );
         }
 
-        public Effect1<A> leftE() {
-            return a -> handle(Either.<A, B>left(a));
+        public Effect1<Tried<A>> leftE() {
+            return a -> handle( Either.<Tried<A>, Tried<B>>left( a ) );
         }
 
-        public Effect1<B> rightE() {
-            return b -> handle(Either.<A, B>right(b));
+        public Effect1<Tried<B>> rightE() {
+            return b -> handle( Either.<Tried<A>, Tried<B>>right( b ) );
         }
     }
 }
---
+++ src/main/java/no/kantega/effect/Tried.java
@@ -0,0 +1,55 @@
+package no.kantega.effect;
+
+import fj.F;
+import fj.P2;
+import fj.data.Either;
+import fj.data.Option;
+
+import static fj.P.p;
+
+public class Tried<A> {
+
+    final Either<Throwable, A> value;
+
+    private Tried(Either<Throwable, A> value) {
+        this.value = value;
+    }
+
+    public static <A> Tried<A> value(A a) {
+        return new Tried<>( Either.right( a ) );
+    }
+
+    public static <A> Tried<A> fail(Throwable t) {
+        return new Tried<>( Either.left( t ) );
+    }
+
+    public <B> Tried<B> map(F<A, B> f) {
+        return new Tried<>( value.right().map( f ) );
+    }
+
+    public <B> Tried<B> flatMap(F<A, Tried<B>> f) {
+        return fold( Tried::<B>fail, f::f );
+    }
+
+
+    public <B> Tried<P2<A,B>> and(Tried<B> other){
+        return flatMap( valueA -> other.map( valueB -> (p( valueA, valueB )) ) );
+    }
+
+    public boolean isValue() {
+        return value.isRight();
+    }
+
+    public boolean isThrowable() {
+        return value.isLeft();
+    }
+
+    public Option<A> toOption() {
+        return value.right().toOption();
+    }
+
+    public <X> X fold(F<Throwable, X> g, F<A, X> f) {
+        return value.either( g, f );
+    }
+
+}
