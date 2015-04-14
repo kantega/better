a29f540588d994588bd935597e98b26c74297920
Atle Prange <atle.prange@kantega.no>
2015-04-14 09:15:23 +0200

    cosmetics

--- src/main/java/no/kantega/Shows.java
+++ src/main/java/no/kantega/Shows.java
@@ -10,13 +10,16 @@ import static fj.data.Stream.fromString;
 public class Shows {
 
 
-    public static <A> Show<List<A>> listShow(Show<A> sa,String prefix, String delim,String postfix){
-        return Show.show( as -> sreamShow( sa,prefix,delim,postfix ).show( as.toStream() ) );
+    public static <A> Show<List<A>> listShow(Show<A> sa, String prefix, String delim, String postfix) {
+        return Show.show( as -> sreamShow( sa, prefix, delim, postfix ).show( as.toStream() ) );
     }
 
-    public static <A> Show<Stream<A>> sreamShow(Show<A> sa,String prefix, String delim,String postfix){
-        return Show.<Stream<A>>show( as -> Stream.join( as.map( sa.show_() ).intersperse( fromString( delim ) ).cons( fromString( prefix ) ).snoc( p( fromString( postfix ) ) ) )
-        );
+    public static <A> Show<Stream<A>> sreamShow(Show<A> sa, String prefix, String delim, String postfix) {
+        return Show.<Stream<A>>show( as ->
+                Stream.join( as.map( sa.show_() )
+                        .intersperse( fromString( delim ) )
+                        .cons( fromString( prefix ) )
+                        .snoc( p( fromString( postfix ) ) ) ) );
 
     }
 
